ac9498dc78559d341848ff514c0658cfa44d5d4b
santoshraja1312 <99283770+santoshraja1312@users.noreply.github.com>
2022-02-09 00:30:20 +0530

    Update simplestorage.sol

--- simplestorage.sol
+++ simplestorage.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity >=0.6.0 <0.9.0;
 
 contract simplestorage
@@ -8,10 +10,12 @@ contract simplestorage
    }
 
    people[] public ppl;
+   mapping(string => uint256) public maps2n;
 
    function addperson (uint256 _num , string memory _name) public{
 
        ppl.push(people(_num ,_name));
+       maps2n [_name] = _num;
        
    }
 
